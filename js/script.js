99ed11a2d228618994a7d9bccb899e0e38ccadea
Julie Park <jspark721@gmail.com>
2019-04-04 16:14:49 -0700

    code menu icon with pure css

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ let menu = document.querySelector('.navbar-main-menu');
 
 close.addEventListener('click', function() {
   menu.classList.toggle('close');
+  open.classList.toggle('close');
 });
 
 open.addEventListener('click', function() {
   menu.classList.toggle('close');
+  open.classList.toggle('close');
 });
