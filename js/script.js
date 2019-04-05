c87db083da8e35917cd09365917e5243103d7681
Julie Park <jspark721@gmail.com>
2019-04-05 16:59:21 -0700

    finish landing section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let close = document.querySelector('.nav-close-icon');
 let open = document.querySelector('.navbar-open-icon')
 let menu = document.querySelector('.nav-main');
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
 
 close.addEventListener('click', function() {
   menu.classList.toggle('close');
@@ -11,3 +13,17 @@ open.addEventListener('click', function() {
   menu.classList.toggle('close');
   open.classList.toggle('nodisplay');
 });
+
+
+window.onscroll = function() {
+  stickyFunction();
+};
+
+
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
