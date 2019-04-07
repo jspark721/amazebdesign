311171907c45ebd6d453509e2ed3282dbefeed27
Julie Park <jspark721@gmail.com>
2019-04-07 03:28:51 -0700

    update landing page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 let close = document.querySelector('.nav-close-icon');
 let open = document.querySelector('.navbar-open-icon')
 let menu = document.querySelector('.nav-main');
-let navbar = document.getElementById("navbar");
+let navbar = document.getElementById('navbar');
 let sticky = navbar.offsetTop;
 
 close.addEventListener('click', function() {
   menu.classList.toggle('close');
   open.classList.toggle('nodisplay');
+  navbar.style.display = 'grid';
 });
 
 open.addEventListener('click', function() {
   menu.classList.toggle('close');
   open.classList.toggle('nodisplay');
+  navbar.style.display = 'none';
 });
 
 
@@ -22,8 +24,8 @@ window.onscroll = function() {
 
 function stickyFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add('sticky')
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove('sticky');
   }
 }
