a6100e915a0c4d01da1718a7a36ef7bcca397ea3
Julie Park <jspark721@gmail.com>
2019-04-08 16:26:31 -0700

    update navbar section javascript

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let close = document.querySelector('.nav-close-icon');
 let open = document.querySelector('.navbar-open-icon')
 let menu = document.querySelector('.nav-main');
 let navbar = document.getElementById('navbar');
+let logo = document.querySelector('.logo');
+let order = document.querySelector('.navbar-order');
 let sticky = navbar.offsetTop;
 
 close.addEventListener('click', function() {
@@ -24,7 +26,10 @@ window.onscroll = function() {
 
 function stickyFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add('sticky')
+    navbar.classList.add('sticky');
+    // navbar.style.backgroundColor = '#FFFFFF';
+    logo.classList.remove('nodisplay');
+    order.classList.remove('nodisplay');
   } else {
     navbar.classList.remove('sticky');
   }
